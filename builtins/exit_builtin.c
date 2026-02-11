ffb731ca9d0a61084ffb56fd4ae9695336645bb1
taosa <tomasafonso12050@gmail.com>
2026-02-11 20:37:29 +0000

    Exit builtin fix

--- builtins/exit_builtin.c
+++ builtins/exit_builtin.c
@@ -6,7 +6,7 @@
 /*   By: tafonso <tafonso@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 16:19:47 by mirandsssg        #+#    #+#             */
-/*   Updated: 2026/01/14 19:17:10 by tafonso          ###   ########.fr       */
+/*   Updated: 2026/02/11 18:05:35 by tafonso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,26 +63,38 @@ static int	ft_strdigit(char *str)
 	return (1);
 }
 
-int	exit_builtin(t_data *data, t_cmd *cmd)
+static int	exit_checks(t_data *data, t_cmd *cmd)
 {
-	int	status;
-
-	if (!cmd)
-		cmd = data->cmd;
-	status = 0;
 	if (cmd->args[1] && cmd->args[2])
 	{
-		fprintf(stderr, "%s: too many arguments\n", cmd->args[0]);
+		ft_putstr_fd(cmd->args[0], 2);
+		ft_putstr_fd(": to many arguments\n", 2);
 		data->last_exit_status = 1;
 		return (1);
 	}
 	if (cmd->args[1] && ft_strdigit(cmd->args[1]) == 0)
 	{
-		fprintf(stderr, "exit: %s: numeric argument required\n", cmd->args[1]);
+		ft_putstr_fd("exit: ", 2);
+		ft_putstr_fd(cmd->args[1], 2);
+		ft_putstr_fd(": numeric argument required\n", 2);
 		data->should_exit = 1;
 		printf("exit\n");
 		return (data->last_exit_status = 2, 2);
 	}
+	return (0);
+}
+
+int	exit_builtin(t_data *data, t_cmd *cmd)
+{
+	int	status;
+	int	ret;
+
+	if (!cmd)
+		cmd = data->cmd;
+	status = 0;
+	ret = exit_checks(data, cmd);
+	if (ret != 0)
+		return (data->last_exit_status);
 	if (cmd->args && cmd->args[1])
 		status = ft_atoi(cmd->args[1]);
 	else
